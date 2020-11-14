3aee3340e9be5de9cd5eede508dd0992f12e4dbf
Jonathan Honda <hondahelix120@gmail.com>
2020-11-13 16:37:27 -0800

    configured app and made header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+//import react from "react";
+
+function Header(){
+    return(
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4 d-flex justify-content-center">Employee Directory</h1>
+            <p className="lead d-flex justify-content-center">Helps you keep track of your employees!!!</p>
+        </div>
+    </div>
+    );
+}
+
+export default Header;
